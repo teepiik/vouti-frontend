eafdb95750ee37ba5cb49765c233c0c23b1aac69
teepiik <teemu.piik@gmail.com>
2020-07-17 11:20:33 +0300

    more configurations

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,16 @@
-import React from 'react';
-import './App.css';
+import React from 'react'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Hello Vouti
-        </p>
+    <div className='Container'>
+      <header className='App-header'>
+        <p>Hello Vouti</p>
       </header>
     </div>
-  );
+  )
 }
 
-export default App;
+const voutiText =
+  'Voudit kuuluivat keskiajan ylhäisaateliin. Heidän tehtäviään oli periä kuninkaalle menevät verot sekä toimia käskynhaltijana tietyllä alueella tai linnassa linnanisännän apulaisena. Esimerkiksi Turun linnanvoutina toimi vuonna 1515 laamanni Klaus Henrikinpojan poika Krister.'
+
+export default App
