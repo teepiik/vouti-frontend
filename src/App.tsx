7e38e598fc46f35a0c370047a39e12c60f04b8d2
teepiik <teemu.piik@gmail.com>
2020-07-18 13:58:04 +0300

    background img added

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import { Header } from './Components/Header'
 import { DebtListing } from './Components/DebtListing'
 import { Debt } from './types'
 import debtService from './Services/debts'
+import Background from './bgpic.png'
 
 const App: React.FC = () => {
   const [debts, setDebts] = useState<Array<Debt>>([])
@@ -34,6 +35,8 @@ const GlobalStyle = createGlobalStyle`
     padding: 0px;
     line-height: 1.5;
     position: relative;
+    background-image: url(${Background});
+    background-color: #cccccc;
   }
 
   div {
