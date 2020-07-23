96015d0adb64b86aaa1ef9dd3e88ba5a642fa17f
teepiik <teemu.piik@gmail.com>
2020-07-23 18:35:39 +0300

    card includes all information now

--- src/Components/DebtCard.tsx
+++ src/Components/DebtCard.tsx
@@ -9,13 +9,27 @@ interface Props {
 
 export const DebtCard: React.FC<Props> = ({ item }) => {
   return (
-    <Card>
-      <p>{item?.debt_item}</p>
-    </Card>
+    <CardWrapper>
+      <DebtItem>{item?.debt_item}</DebtItem>
+      <People>
+        Deptor: {item?.debtor}, Receiver: {item?.receiver}
+      </People>
+      <Dates>
+        Date of origin: {item?.date_origin}, Paid: {item?.date_paid}
+      </Dates>
+      <Comment>Comments: {item?.comment}</Comment>
+    </CardWrapper>
   )
 }
 
-const Card = styled.div`
+const CardWrapper = styled.div`
   box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);
   background-color: saddleBrown;
+  padding: 0.5rem;
+  margin-bottom: 1rem;
 `
+
+const DebtItem = styled.h2``
+const People = styled.p``
+const Dates = styled.p``
+const Comment = styled.p``
